96b4e732f489c84dabc7fd4e2f899e928550185f
Guillermo Otegui <guilleotegui03@gmail.com>
2025-02-06 09:51:34 -0300

    fix: build

---
+++ rollup.config.mjs
@@ -0,0 +1,34 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    postcss({
+      extract: true, // Esto genera un archivo CSS separado en la carpeta 'dist'
+      minimize: true, // Minimiza el CSS
+      sourceMap: true, // Incluye sourcemaps para debugging
+    }),
+    typescript(),
+    terser(),
+  ],
+};
